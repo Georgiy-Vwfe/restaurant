4d154c4e787a48ff4acb54f09c5f53688b945e76
Student <student@c004.local>
2018-11-23 15:14:25 +0300

    MenuTableViewControlle

--- restaurant/Controllers/ViewControllers/MenuItemDetailViewController.swift
+++ restaurant/Controllers/ViewControllers/MenuItemDetailViewController.swift
@@ -2,21 +2,50 @@
 //  MenuItemDetailViewController.swift
 //  restaurant
 //
-//  Created by Student on 13/11/2018.
+//  Created by Student on 23/11/2018.
 //  Copyright © 2018 Student. All rights reserved.
 //
 
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var detailTextLabel: UILabel!
+    @IBOutlet weak var addToOrderButton: UIButton!
+    
+    var menuItem: MenuItem! = MenuItem(
+    id: 0,
+    name: "text `fahjdfvhjk`dsfas`vfhjkcdv`jshvcfhjasvcjhv`aschbadjsgfhjsdafvjkh`asvfljchb",
+    detailText: "TEXT ;SKJG;SDJGFLJSDZHFHGDSKLJF';SAEJ;DGHZKLJFHKJDSHFKLJSZDH",
+    price: 9.99,
+    category: "HGIUHSD",
+    imageURL: URL(string: "https://apple.com")!
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        addToOrderButton.layer.cornerRadius = 5
+        
+        updateUI()
     }
     
+    func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$%.2f", menuItem.price)
+        detailTextLabel.text = menuItem.detailText
+        
+    }
 
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            sender.transform = CGAffineTransform(scaleX: 3, y: 3)
+            sender.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }
+    }
     /*
     // MARK: - Navigation
 
